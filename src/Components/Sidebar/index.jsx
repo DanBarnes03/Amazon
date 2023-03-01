40da7ceeb49eb59a46f7a160bd8b3471d9014761
Dan  Barnes <124290123+DanBarnes03@users.noreply.github.com>
2023-03-01 21:44:37 +0000

    Add files via upload

---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './index.css';
+
+export const Sidebar = () => {
+    return(
+        <div class='sideNav'>
+            <a href='#'>- All</a>
+            <a href='#'>- Buy Again</a>
+            <a href='#'>- Prime</a>
+            <a href='#'>- Morrison's</a>
+            <a href='#'>- Prime Video</a>
+            <a href='#'>- Free Delivery</a>
+            <a href='#'>- Browsing History</a>
+            <a href='#'>- Gift Cards & Top Up</a>
+            <a href='#'>- Vouchers</a>
+            <a href='#'>- John's Amazon</a>
+            <a href='#'>- Health & Personal Care</a>
+            <a href='#'>- Today's Deals</a>
+            <a href='#'>- Fashion</a>
+            <a href='#'>- Home & Garden</a>
+            <a href='#'>- Shopper Toolkit</a>
+            <a href='#'>- Beauty</a>
+            <a href='#'>- Toys & Games</a>
+            <a href='#'>- Contact Us</a>
+            <a href='#'>- About Us</a>
+        </div>
+    )
+};
---
+++ src/Components/Slideshow/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import './index.css';
+
+const Thumbnail = ({ arr, image, index }) => {
+  return (
+  <div className="thumbnail">
+    {arr.map((imgsrc, i) => (
+        <img 
+        key={i} 
+        height='50' 
+        src={imgsrc}
+        onClick={() => image(i)}
+        className={index === i ? 'active' : ''}
+        />
+        ))}
+  </div>
+  )
+}
+
+export const Slideshow = ({imgs}) => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    setIndex(0)
+  }, [])
+
+  const next = () => {
+    if(index === imgs.length -1){
+      setIndex(0)
+    } else{
+      setIndex(index +1)
+    }
+  }
+
+  const prev = () => {
+    if(index === 0){
+      setIndex(imgs.length -1)
+    } else {
+      setIndex(index -1)
+    }
+  }
+
+  return <div className='slideshow'>
+    <img className='mainImg' src={imgs[index]}/>
+    <div className='actions'>
+      <button class='arrowButton' onClick={prev}>←</button>
+      <button class='arrowButton' onClick={next}>→</button>
+    </div>
+    <Thumbnail arr={imgs} image={setIndex} index={index}/>
+  </div>
+};
